697abc1396cfc7e9f0064ac4d5d412074f62a561
Mohamed Shehta <52452940+MuShehta@users.noreply.github.com>
2026-07-18 15:06:31 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+public class test { 
+public decimal CalculateDiscount(decimal amount)
+{
+    if (amount > 100m)
+        return amount * 0.15m;
+
+    if (amount > 500m && amount < 1000m)
+        return amount * 0.10m;
+
+    return 0m;
+}
+}
