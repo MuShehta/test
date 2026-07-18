77eff697d9f3bdeafdd200cfed24df9496b968b8
Mohamed Shehta <52452940+MuShehta@users.noreply.github.com>
2026-07-18 15:15:09 +0300

    Add files via upload

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,17 @@
+﻿namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public decimal CalculateDiscount(decimal amount)
+        {
+            if (amount > 100m)
+                return amount * 0.15m;
+
+            if (amount > 500m && amount < 1000m)
+                return amount * 0.10m;
+
+            return 0m;
+        }
+
+    }
+}
